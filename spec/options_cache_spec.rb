c769ef9a7d455cfec1831309e15684027c03b6d9
merten <merten.falk@crossengage.io>
2022-06-21 19:19:45 +0200

    Test for option cache

--- spec/options_cache_spec.rb
+++ spec/options_cache_spec.rb
@@ -1,9 +1,20 @@
 require "spec_helper"
 
 describe MultiJson::OptionsCache do
-  before { described_class.reset }
+  describe 'dump something' do
+    it 'does not raise a error' do
+      described_class.instance_variable_set(:@dump_cache, nil)
+      described_class.instance_variable_set(:@load_cache, nil)
+
+      expect do
+        MultiJson.dump({ foo: 'bar' }, mode: :rails, adapter: :oj)
+      end.not_to raise_error
+    end
+  end
 
   it "doesn't leak memory" do
+    described_class.reset
+
     described_class::MAX_CACHE_SIZE.succ.times do |i|
       described_class.fetch(:dump, :key => i) do
         { :foo => i }
